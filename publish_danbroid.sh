550ff86a77bd36fce330ecd23f623b1cd8d78c42
Dan Brough <dan@danbrough.org>
2022-04-20 11:17:29 +1200

    update

--- publish_danbroid.sh
+++ publish_danbroid.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-./gradlew -PLOCAL_MAVEN_REPO=file:///mnt/files2/repo/ \
-	publishAllPublicationsToMavenRepository  && syncmaven 
-
+./gradlew -PLOCAL_MAVEN_REPO=file:///mnt/files2/cache/repo/ publishAllPublicationsToMavenRepository && syncmaven 
