fd466da8f82f2e63cc8062c6f672f17836feef34
Dan Brough <dan@mac.local>
2022-09-28 15:57:06 +1300

    update

--- publish_dan.sh
+++ publish_dan.sh
@@ -5,6 +5,9 @@ cd "$(dirname "$0")"
 [ -z "$SONA_REPO_ID" ] && echo SONA_REPO_ID not set && exit 1
 
 TASKS=publishAllPublicationsToMavenRepository
+if [ "$(uname)" = "Darwin" ]; then 
+	TASKS="$(cat mac_targets.txt)"
+fi
 
 ./gradlew -PsignPublications \
   $TASKS
