673e15a817e7394a4e97281f6b5ec64844053d29
Dan Brough <dan@danbrough.org>
2022-09-30 15:40:52 +1300

    update

--- buildSrc/src/main/kotlin/Publishing.kt
+++ buildSrc/src/main/kotlin/Publishing.kt
@@ -45,7 +45,7 @@ fun mavenRepositoryUri(): URI {
     return if (repositoryId == null) {
         URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
     } else {
-        URI("https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId".also{println("MAVEN URL: $it")})
+        URI("https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId")
     }
 }
 
@@ -53,8 +53,8 @@ fun configureMavenPublication(rh: RepositoryHandler, project: Project) {
     rh.maven {
         url = mavenRepositoryUri()
         credentials {
-            username = project.getSensitiveProperty("libs.sonatype.user")
-            password = project.getSensitiveProperty("libs.sonatype.password")
+            username = project.properties["libs.sonatype.user"].toString().trim()
+            password = project.properties["libs.sonatype.password"].toString().trim()
         }
     }
 
